3e1113690477f0c0cd5363f27803142eae50adb7
JPedroPV <jpedropv@outlook.com>
2022-02-12 19:31:50 -0600

    7:30pm

--- src/App.js
+++ src/App.js
@@ -4,27 +4,46 @@ import React, { useState} from 'react';
 
 
 function App() {
-  
-function add() {
-  setCount((prev) => { return prev + 1});
-}
-function minus() {
-  setCount((prev) => { return prev - 1});
-}
-function reset() {
-  setCount((prev) => { return 0})
-}
-  const [count, setCount] = useState(0);
+  const [stock, setStock] = useState("AAPL")
+  function changeAmount(e) {
+    setAmount(e.target.value)
+  }
+  const [amount, setAmount] = useState(0)
+  function changeStock(e) {
+    setStock(e.target.value)
+  }
+  const [start, setStart] = useState("YYYY-MM-DD")
+  function changeStart(e) {
+    setStart(e.target.value)
+  }
+  const [end, setEnd] = useState("YYYY-MM-DD")
+  function changeEnd(e) {
+    setEnd(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          { count }
-        </p>
-        <button className="my-button" onClick={add}>+</button>
-        <button className="my-button" onClick={minus}>-</button>
-        <button className="my-button" onClick={reset}>reset</button>
-      </header>
+      <body className='lbody'>
+        <label for="amount">Stock Amount: </label>
+        <input className="date-in" type="text" id="amount" value={ amount } onChange= {(e) => changeAmount(e)}></input><br></br><br></br>
+        <label for="stock">Stock Name: </label>
+        <input className="date-in" type="text" id="stock" value={ stock } onChange= {(e) => changeStock(e)}></input><br></br><br></br>
+        <label for="startDate">Start Date: </label>
+        <input className="date-in" type="text" id="startDate" value={ start } onChange= {(e) => changeStart(e)}></input><br></br><br></br>
+        <label for="endDate">End Date: </label>
+        <input className="date-in" type="text" id="endDate" value={ end } onChange= {(e) => changeEnd(e)}></input>
+        <p>If you bought {amount} stock(s) of {stock} in {start}, later, in {end}<br></br> it would be worth $_____</p>
+      </body>
+      <body className='rbody'>
+        <label for="amount">Stock Amount: </label>
+        <input className="date-in" type="text" id="amount" value={ amount } onChange= {(e) => changeAmount(e)}></input><br></br><br></br>
+        <label for="stock">Stock Name: </label>
+        <input className="date-in" type="text" id="stock" value={ stock } onChange= {(e) => changeStock(e)}></input><br></br><br></br>
+        <label for="startDate">Start Date: </label>
+        <input className="date-in" type="text" id="startDate" value={ start } onChange= {(e) => changeStart(e)}></input><br></br><br></br>
+        <label for="endDate">End Date: </label>
+        <input className="date-in" type="text" id="endDate" value={ end } onChange= {(e) => changeEnd(e)}></input>
+        <p>If you bought {amount} stock(s) of {stock} in {start}, later, in {end}<br></br> it would be worth $_____</p>
+      </body>
     </div>
   );
 }
